32c44c26fae0e3a9970e9a4d57c8cb9975405f9a
John Doe <surenbound@yandex.ru>
2024-02-09 17:07:18 +0300

    init

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,7 @@
 import Router from 'express'
 // import axios from 'axios'
 // import fs from 'fs'
-import { PrismaClient, User, Role } from '@prisma/client'
-const prisma = new PrismaClient()
+
 const router = Router()
 
 // Описываем функцию, которая будет обрабатывать GET запросы на адрес '/'
@@ -10,28 +9,4 @@ router.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-
-async function importData() {
-  try {
-    const sneakersData = [
-    ];
-
-    for (const sneakerData of sneakersData) {
-      await prisma.sneakerData.create({
-        data: {
-          name: sneakerData.title,
-          img: sneakerData.imageUrl,
-          price: sneakerData.price,
-        }
-      });
-    }
-    console.log('Data imported successfully');
-  } catch (error) {
-    console.error('Error importing data:', error);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-importData();
 export default router
